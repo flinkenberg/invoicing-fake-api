ec9822d776ebd32fef60c66f6d5011a7e6ecdf97
flinkenberg <hello@ondrejstastny.com>
2018-07-06 00:39:32 +0100

    Add single id endpoint

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,32 +1,43 @@
 import faker from "faker";
 
-const data = Array(400).fill().map(((_, i) => ({
-    id: i,
-    name: faker.name.firstName(),
-})));
-
 const router = (app, config) => {
+    const data = Array(400).fill().map(((_, i) => ({
+        id: i+1,
+        name: faker.name.firstName(),
+    })));
     let timeout = null;
     const tryDelay = action => {
-        timeout && clearInterval(timeout);
-        config && config.delay && action
-            ? timeout = setTimeout(() => action(), config.delay)
-            : action()
+        if (timeout) clearInterval(timeout);
+        if (config && config.delay && action) {
+            timeout = setTimeout(() => action(), config.delay);
+        } else {
+            action();
+        }
     };
     app.get("/", (_req, res) => (
         tryDelay(() => res.status(200).send("/"))
-    )),
+    ));
     app.get("/contacts", (_req, res) => {
-        const results = Array(50).fill().map(((_, i) => ({
-            id: i,
-            name: faker.name.firstName(),
-        })));
         res.status(200);
-        tryDelay(() => res.send({
-            count: results.length,
-            results,
-        }));
-    })
+        tryDelay(() => res.send(data));
+    });
+    app.get("/contacts/:id", (req, res) => {
+        const id = req.params.id;
+        if (isFinite(id) && id > 0) {
+            const match = data.find(item => item.id === parseInt(id, 10));
+            if (match) {
+                tryDelay(() => res.status(200).send(match));
+            } else {
+                tryDelay(() => res.status(404).send({
+                    message: "ID not found."
+                }));
+            }
+        } else {
+            tryDelay(() => res.status(400).send({
+                    message: "ID parameter has wrong format.",
+            }));
+        }
+    });
 };
 
 export default router;
