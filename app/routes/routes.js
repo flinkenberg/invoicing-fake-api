31e5baffe22b96c81357862238c08b1bee085774
flinkenberg <hello@ondrejstastny.com>
2018-07-12 01:58:25 +0100

    Add full text query filtering

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,9 +1,18 @@
 import faker from "faker";
 
 const router = (app, config) => {
-    const data = Array(400).fill().map(((_, i) => ({
+    const fakeData = Array(400).fill().map(((_, i) => ({
         id: i+1,
-        name: faker.name.firstName(),
+        name: faker.company.companyName(),
+        email: faker.internet.email(),
+        phone: faker.phone.phoneNumber(),
+        address: faker.fake("{{address.streetAddress}}, {{address.city}}, {{address.zipCode}}"),
+        company_reg_id: "123456",
+        tax_id: "123456",
+        account_number: "12345678",
+        sort_code: "123456",
+        bank_name: faker.company.companySuffix(),
+        notes: faker.lorem.sentence(),
     })));
     let timeout = null;
     const tryDelay = action => {
@@ -14,31 +23,42 @@ const router = (app, config) => {
             action();
         }
     };
+    const getPage = (data, page) => {
+        const pageLimit = config && config.pageLimit ? config.pageLimit : 10;
+        return data.slice((pageLimit * page) - pageLimit, pageLimit * page);
+    }
+    const getFiltered = (data, query) => {
+        return data.filter(item => item.name.toLowerCase().startsWith(query.toLowerCase()));
+    }
     app.get("/", (_req, res) => (
         tryDelay(() => res.status(200).send("/"))
     ));
     app.get("/contacts", (req, res) => {
+        let results = fakeData;
+        if (req.query.q) {
+            results = getFiltered(results, req.query.q);
+        }
         if (req.query.page) {
-            const pageLimit = config && config.pageLimit ? config.pageLimit : 10;
-            const results = data.slice((pageLimit * req.query.page) - pageLimit, pageLimit * req.query.page);
-            if (results.length) {
-                tryDelay(() => res.status(200).send({
-                    count: data.length,
-                    results,
-                }));
-            } else {
-                tryDelay(() => res.status(404).send({
-                    message: "Page does not exist."
-                }));
-            }
+            results = getPage(results, req.query.page);    
+        }
+        if (req.query.limit) {
+            results = results.slice(0, req.query.limit);
+        }
+        if (results.length) {
+            tryDelay(() => res.status(200).send({
+                count: fakeData.length,
+                results,
+            }));
         } else {
-            tryDelay(() => res.status(200).send(data));
+            tryDelay(() => res.status(404).send({
+                message: "No results."
+            }));
         }
     });
     app.get("/contacts/:id", (req, res) => {
         const id = req.params.id;
         if (isFinite(id) && id > 0) {
-            const match = data.find(item => item.id === parseInt(id, 10));
+            const match = fakeData.find(item => item.id === parseInt(id, 10));
             if (match) {
                 tryDelay(() => res.status(200).send(match));
             } else {
